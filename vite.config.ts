ffeafae692f57f2eaeca3075d9c176d257000231
horaciothalles-stack <horaciothalles@gmail.com>
2026-06-26 11:26:22 -0300

    use tanstackStart plugin vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
     tsConfigPaths(),
-    cloudflare(),
+    tanstackStart(),
   ],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
